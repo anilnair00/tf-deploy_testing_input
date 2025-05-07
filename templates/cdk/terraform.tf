20fd58bc023b2ebca20cdf4a99a854316f6a9e43
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-07 07:56:50 +0000

    Update terraform.tf

--- templates/cdk/terraform.tf
+++ templates/cdk/terraform.tf
@@ -1,13 +1,12 @@
 terraform {
   required_version = ">= 1.9"
-  backend "s3" {
-    bucket         = "ac-harness-resources-terraform-state"
-    encrypt        = true
-    key            = "harness/templates/cdk/ac-it-notifications-harness-resources-tf/ac-it-notifications-harness-resources-tf.tfstate"
-    dynamodb_table = "ac-tools-tf-prod-cac1-state-lock"
-    region         = "ca-central-1"
-    role_arn       = var.S3_BACKEND_ROLE_ARN
+  backend "azurerm" {
+    resource_group_name  = "Testvm_group"
+    storage_account_name = "testvmgroupb7e2"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
   }
+}
   required_providers {
     harness = {
       source = "harness/harness"
